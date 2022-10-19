9e061088f8a3004e275ab6a96e661f82985f3dc5
konishi333 <jobhuntingkonishi3@gmail.com>
2022-10-19 17:11:12 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:create]
+
+  def create
+    prototype = Prototype.find(params[:prototype_id])
+    comment = prototype.comments.new(comment_params)
+
+    if comment.save
+      redirect_to prototype_path(comment.prototype_id)
+    else
+      @prototype = Prototype.find(params[:prototype_id])
+      @comment = Comment.new
+      render "prototypes/show"
+    end
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+  end
+
+end
